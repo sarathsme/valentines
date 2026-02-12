4d55f7bac6701a2d458b69e51af6d56b47e65493
TO-SMenon <sarath.menon@dirs21.team>
2026-02-12 16:34:45 +0100

    build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
+  app: {
+    baseURL: '/valentines/'
+  },
+  nitro: {
+    output: {
+      publicDir: 'docs'
+    }
+  },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true }
 })
